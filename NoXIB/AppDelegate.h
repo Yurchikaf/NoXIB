945911bfd6b3a5d107c804b273c382370c0ae975
Yurchikaf <macpro13@andreyy.home>
2014-05-24 17:01:11 -0400

    Initial Commit

---
+++ NoXIB/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NoXIB
+//
+//  Created by Andrey on 5/24/14.
+//  Copyright (c) 2014 EnjoyTheApp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NoXIBTests/NoXIBTests.m
@@ -0,0 +1,34 @@
+//
+//  NoXIBTests.m
+//  NoXIBTests
+//
+//  Created by Andrey on 5/24/14.
+//  Copyright (c) 2014 EnjoyTheApp. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface NoXIBTests : XCTestCase
+
+@end
+
+@implementation NoXIBTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
